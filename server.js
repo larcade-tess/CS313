34b30a6003b3f5bef1d6655aa64569eddb138749
Tess Larcade <tesslarcade@gmail.com>
2018-03-16 20:43:05 -0600

    updated Server

--- server.js
+++ server.js
@@ -3,13 +3,47 @@ var app = express();
 
 var pg = require("pg"); // This is the postgres database connection module.
 
-var dbURL = getenv('DATABASE_URL');
-if (process.env == "herokudb"){
-	const connectionString = process.env.dbURL;
-}
-else {
-	const connectionString = "postgres://admin:1234pass@localhost:5433/japdb";
-}
+
+
+
+
+
+var db_url = url.parse(process.env.DATABASE_URL);
+
+var scheme = db_url.protocol.substr(0, db_url.protocol.length - 1);
+var user = db_url.auth.substr(0, db_url.auth.indexOf(':'));
+var pass = db_url.auth.substr(db_url.auth.indexOf(':') + 1, db_url.auth.length);
+var host = db_url.host.substr(0, db_url.host.indexOf(':'));
+var port = db_url.host.substr(db_url.host.indexOf(':') + 1, db_url.host.length);
+var db = db_url.path.substr(db_url.path.indexOf('/') + 1, db_url.path.length);
+
+const client = new Client({
+   host: host,
+   user: user,
+   database: db,
+   password: pass,
+   port: port
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var dbURL = getenv('DATABASE_URL');
+// if (process.env == "herokudb"){
+// 	const connectionString = process.env.dbURL;
+// }
+// else {
+// 	const connectionString = "postgres://admin:1234pass@localhost:5433/japdb";
+// }
 
 app.set('port', (process.env.PORT || 5000));
 
