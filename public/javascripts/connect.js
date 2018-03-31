e25a2f8128e9cdc537e7ba4fe3a9f126e67d6295
Tess Larcade <tesslarcade@gmail.com>
2018-03-30 21:11:21 -0600

    Connected pages to server

--- public/javascripts/connect.js
+++ public/javascripts/connect.js
@@ -10,14 +10,21 @@ function allAp(){
 				
 				for(var i=0; i < jobj.length; i++){
 					var element = document.createElement('div');
+					var id = jobj[i].apartment_id;
 					var location = jobj[i].location;
 					var rented = jobj[i].rented;
 					var price= jobj[i].price;
 					var bed = jobj[i].bed;
 					var bath = jobj[i].bath;
 					var sqft = jobj[i].sqft;
-					var text = document.createTextNode(location + " " + rented + " " + price + " " + bed + " " + bath + " " + sqft);
+					var text = document.createTextNode(location + " " + rented + " " + price + " " + bed + " " + bath + " " + sqft + " ");
+					var link = document.createElement('a');
+					var linkto = "/editApt?id="+ id;
+					var edittxt = "Edit";
+					link.setAttribute("href", linkto);
+					link.append(edittxt);
 					element.append(text);
+					element.append(link);
 					parent.append(element);
 				}
 			}
@@ -31,3 +38,188 @@ function allAp(){
 	xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 	xhttp.send();
 }
+
+function apartment(){
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4) {
+			if (this.status == 200) {
+				var jobj = JSON.parse(this.responseText);
+				console.log(this.responseText);
+				var parent = document.getElementById('container');
+				
+				
+				for(var i=0; i < jobj.length; i++){
+					var element = document.createElement('div');
+					var id = jobj[i].apartment_id;
+					var location = jobj[i].location;
+					var rented = jobj[i].rented;
+					var price= jobj[i].price;
+					var bed = jobj[i].bed;
+					var bath = jobj[i].bath;
+					var sqft = jobj[i].sqft;
+					var link = document.createElement('a');
+					var txtlocation = document.createTextNode(location);
+					var linkto = "/apartment"+ id;
+					link.setAttribute("href", linkto);
+					link.append(txtlocation);
+					element.append(link);
+					var text = document.createTextNode(price + " " + bed + " " + bath + " " + sqft);
+					element.append(text);
+					parent.append(element);
+				}
+			}
+			else {
+				console.log('not cool');
+			}
+		}
+	}
+	var requestURL = "available";
+	xhttp.open("GET", requestURL, true);
+	xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	xhttp.send();
+}
+
+function allContacts(){
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4) {
+			if (this.status == 200) {
+				var jobj = JSON.parse(this.responseText);
+				console.log(this.responseText);
+				var parent = document.getElementById('displayContacts');
+				
+				
+				for(var i=0; i < jobj.length; i++){
+					var element = document.createElement('div');
+					var fName = jobj[i].first_name;
+					var lName = jobj[i].last_name;
+					var phone = jobj[i].phone;
+					var email = jobj[i].email;
+
+					var text = document.createTextNode(fName + " " + lName + " " + phone + " " + email);
+					element.append(text);
+					parent.append(element);
+				}
+			}
+			else {
+				console.log('not cool');
+			}
+		}
+	}
+	var requestURL = "getContact";
+	xhttp.open("GET", requestURL, true);
+	xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	xhttp.send();
+}
+
+function apartmentsAvailable(){
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4) {
+			if (this.status == 200) {
+				var jobj = JSON.parse(this.responseText);
+				console.log(this.responseText);
+				var parent = document.getElementById('apartmentSelect');
+				
+				
+				for(var i=0; i < jobj.length; i++){
+					var element = document.createElement('option');
+					var location = jobj[i].location;
+					var id = jobj[i].apartment_id;
+					element.setAttribute("value", id);
+					var text = document.createTextNode(location);
+					element.append(text);
+					parent.append(element);
+				}
+			}
+			else {
+				console.log('not cool');
+			}
+		}
+	}
+	var requestURL = "showingList";
+	xhttp.open("GET", requestURL, true);
+	xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	xhttp.send();
+}
+
+function apartmentNumber(id){
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4) {
+			if (this.status == 200) {
+				var jobj = JSON.parse(this.responseText);
+				console.log(this.responseText);
+				var parent = document.getElementById('apartmentInfo');
+				
+				
+				for(var i=0; i < jobj.length; i++){
+					var element = document.createElement('div');
+					var location = jobj[i].location;
+					var rented = jobj[i].rented;
+					var price= jobj[i].price;
+					var bed = jobj[i].bed;
+					var bath = jobj[i].bath;
+					var sqft = jobj[i].sqft;
+					var text = document.createTextNode("Location: "+ location + " Price: " + price + " Bed: " + bed + " Bath: " 
+						+ bath + " Square feet: " + sqft);
+					element.append(text);
+					parent.append(element);
+				}
+			}
+			else {
+				console.log('not cool');
+			}
+		}
+	}
+	var requestURL = "apartmentNumber?id=" + id;
+	xhttp.open("GET", requestURL, true);
+	xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	xhttp.send();
+}
+function getid(){
+	var windowurl = window.location.href;
+	console.log(window.location.search.substring(1));
+	var id;
+	var query = window.location.search.substring(1);
+	var vars = query.split("&");
+	for (var i=0; i< vars.length; i++) {
+		var pair = vars[i].split("=");
+		if(pair[0] == 'id'){
+			id = pair[1];
+			editApart(id);
+		}
+	}
+	return(false);
+	console.log(id);
+}
+function editApart(id){
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4) {
+			if (this.status == 200) {
+				var jobj = JSON.parse(this.responseText);
+				console.log(this.responseText);
+				var parent = document.getElementById('apartmentSelect');
+
+				for(var i=0; i < jobj.length; i++){
+					document.getElementById('addr').value = jobj[i].location;
+					document.getElementById('price').value = jobj[i].price;
+					document.getElementById('bed').value = jobj[i].bed;
+					document.getElementById('bath').value = jobj[i].bath;
+					document.getElementById('sqft').value = jobj[i].sqft;
+					document.getElementById('rented').value = jobj[i].rented;
+				}
+			}
+			else {
+				console.log('not cool');
+			}
+		}
+	}
+	var requestURL = "apartmentNumber?id=" + id;
+	xhttp.open("GET", requestURL, true);
+	xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	xhttp.send();
+}
+
--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var upload = multer(); // for parsing multipart/form-data
+var session = require('express-session');
 
 var pg = require("pg"); // This is the postgres database connection module.
 
 const connectionString = process.env.DATABASE_URL || "postgres://admin:1234pass@localhost:5433/japdb";
 console.log('Connecting to DB : ', connectionString);
 var v = path.join(__dirname, 'public', 'views');
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -16,52 +23,101 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', v);
 app.set('view engine', 'ejs');
 
+
+app.use(session({
+	name: 'test-session',
+	secret: 'Ill never tell',
+	saveUninitialized: true
+}));
+
 app.get('/getContact', function(request, response) {
 	getContact(request, response);
 });
 app.get('/insertContact', function(request, response) {
 	insertContact(request, response);
 });
+app.get('/contacts', function(request, response) {
+	response.render(path.join('pages', 'contacts'));
+});
+app.post('/newContact', function (request, response) {
+	addContact(request, response);
+});
+app.post('/settings', function (request, response) {
+	settings(request, response);
+});
+
+
 app.get('/getApartment', function(request, response) {
 	getApartment(request, response);
 });
 app.get('/updateApartment', function(request, response) {
 	updateApartment(request, response);
 });
+app.get('/editApartment', function(request, response) {
+	editApartment(request, response);
+});
+app.get('/editApt', function(request, response){
+	response.render(path.join('pages', 'editApt'));
+});
 app.get('/addApartment', function(request, response){
 	response.render(path.join('pages', 'addApartment'));
+});
+app.post('/newApartment', function (request, response) {
+	addApartment(request, response);
+	// response.render(path.join('pages', 'allApartments'));
 });
 app.get('/all', function(request, response) {
 	all(request, response);
 });
-
-app.get('/', function(request, response) {
-	// allApartments(request, response);
+app.get('/allApartments', function(request, response) {
 	response.render(path.join('pages', 'allApartments'));
 });
+
+app.get('/', function(request, response) {
+	response.render(path.join('pages', 'apartments'));
+});
+app.get('/available', function(request, response) {
+	available(request, response);
+});
+app.get('/apartment1', function(request, response) {
+	response.render(path.join('pages', 'apartment1'));
+});
+app.get('/apartment2', function(request, response) {
+	response.render(path.join('pages', 'apartment2'));
+});
+app.get('/apartmentNumber', function(request, response) {
+	apartmentNumber(request, response);
+});
+
+
+app.get('/showing', function(request, response) {
+	response.render(path.join('pages', 'showing'));
+});
+app.get('/showingList', function(request, response) {
+	showingList(request, response);
+});
+
+app.get('/settings', function(request, response) {
+	response.render(path.join('pages', 'settings'));
+});
+
+
 
 app.listen(app.get('port'), function() {
 	console.log('Node app is running on port', app.get('port'));
 });
 
 function getContact(request, response) {
-	var id = request.query.id;
-
-
-	getPersonFromDb(id, function(error, result) {
-
-		if (error || result == null || result.length != 1) {
+	getPeople(function(error, result) {
+		if (error || result == null ) {
 			response.status(500).json({success: false, data: error});
 		} else {
-			var person = result[0];
-			response.status(200).json(result[0]);
+			response.status(200).json(result);
 		}
 	});
 }
 
-function getPersonFromDb(id, callback) {
-	console.log("Contact Info" + id);
-
+function getPeople(callback) {
 	var client = new pg.Client(connectionString);
 
 	client.connect(function(err) {
@@ -71,11 +127,9 @@ function getPersonFromDb(id, callback) {
 			callback(err, null);
 		}
 
-		var sql = "SELECT contact_id, first_name, last_name, phone, email FROM contact WHERE contact_id = $1::int";
-		var params = [id];
+		var sql = "SELECT * FROM contact";
 
-		var query = client.query(sql, params, function(err, result) {
-			// we are now done getting the data from the DB, disconnect the client
+		var query = client.query(sql, function(err, result) {
 			client.end(function(err) {
 				if (err) throw err;
 			});
@@ -85,17 +139,11 @@ function getPersonFromDb(id, callback) {
 				console.log(err);
 				callback(err, null);
 			}
-
-			console.log("Found result: " + JSON.stringify(result.rows));
-
-			// call whatever function the person that called us wanted, giving it
-			// the results that we have been compiling
+			console.log(result.rows);
 			callback(null, result.rows);
 		});
 	});
-
-} // end of getPersonFromDb
-
+} 
 
 function insertContact(request, response) {
 	var first = request.query.first_name;
@@ -148,7 +196,7 @@ function insertContactToDb(first, last, phone, email, callback) {
 		});
 	});
 
-} // end of getPersonFromDb
+}
 
 
 function getApartment(request, response) {
@@ -228,10 +276,9 @@ function allApartments(callback){
 			callback(err, null);
 		}
 
-		var sql = "SELECT * FROM apartment WHERE rented = false";
+		var sql = "SELECT * FROM apartment";
 
 		var query = client.query(sql, function(err, result) {
-			// we are now done getting the data from the DB, disconnect the client
 			client.end(function(err) {
 				if (err) throw err;
 			});
@@ -246,35 +293,17 @@ function allApartments(callback){
 		});
 	});
 }
-function updateApartment(request, response) {
-	// First get the person's id
-	var id = request.query.id;
-	var rented = request.query.rented;
-	var location = request.query.location;
-	var price = request.query.price;
-	var bed = request.query.bed;
-	var bath = request.query.bath;
-	var sqft = request.query.sqft;
-
-	// TODO: It would be nice to check here for a valid id before continuing on...
-
-	// use a helper function to query the DB, and provide a callback for when it's done
-	updateApartmentonDb(id, rented, location, price, bed, bath, sqft, function(error, result) {
-		// This is the callback function that will be called when the DB is done.
-		// The job here is just to send it back.
-
-		// Make sure we got a row with the person, then prepare JSON to send back
-		if (error || result == null || result.length != 1) {
+function showingList (request, response) {
+	apartmentShow(function(error, result) {
+		if (error || result == null ) {
 			response.status(500).json({success: false, data: error});
 		} else {
-			var person = result[0];
-			response.status(200).json(result[0]);
+			response.status(200).json(result);
 		}
 	});
 }
 
-function updateApartmentonDb(id, rented, location, price, bed, bath, sqft, callback) {
-
+function apartmentShow(callback){
 	var client = new pg.Client(connectionString);
 
 	client.connect(function(err) {
@@ -284,11 +313,9 @@ function updateApartmentonDb(id, rented, location, price, bed, bath, sqft, callb
 			callback(err, null);
 		}
 
-		var sql = "UPDATE apartment SET rented = $2, location =$3, price=$4, bed=$5, bath=$6, sqft=$7 WHERE apartment_id = $1::int";
-		var params = [id, rented, location, price, bed, bath, sqft];
+		var sql = "SELECT apartment_id, location FROM apartment WHERE rented = false";
 
-		var query = client.query(sql, params, function(err, result) {
-			// we are now done getting the data from the DB, disconnect the client
+		var query = client.query(sql, function(err, result) {
 			client.end(function(err) {
 				if (err) throw err;
 			});
@@ -298,13 +325,216 @@ function updateApartmentonDb(id, rented, location, price, bed, bath, sqft, callb
 				console.log(err);
 				callback(err, null);
 			}
-
-			console.log("Found result: " + JSON.stringify(result.rows));
-
-			// call whatever function the person that called us wanted, giving it
-			// the results that we have been compiling
+			console.log(result.rows);
 			callback(null, result.rows);
 		});
 	});
+}
 
+function addContact (request, response) {
+	var fName = request.body.fName;
+	var lName = request.body.lName;
+	var phone = request.body.phone;
+	var email = request.body.email;
+
+	newContact(fName, lName, phone, email, (function(error, result) {
+		if (error || result == null ) {
+			response.status(500).json({success: false, data: error}); //make Error page
+		} else {
+			response.render(path.join('pages', 'thanks'));
+		}
+	}));
+}
+
+function newContact(fName, lName, phone, email, callback){
+	var client = new pg.Client(connectionString);
+
+	client.connect(function(err) {
+		if (err) {
+			console.log("Error connecting to DB: ")
+			console.log(err);
+			callback(err, null);
+		}
+		var sql = "INSERT INTO contact (first_name, last_name, phone, email) Values ($1, $2, $3, $4)";
+		var params = [fName, lName, phone, email];
+
+		var query = client.query(sql, params, function(err, result) {
+			client.end(function(err) {
+				if (err) throw err;
+			});
+
+			if (err) {
+				console.log("Error in query: ")
+				console.log(err);
+				callback(err, null);
+			}
+			console.log(result.rows);
+			callback(null, result.rows);
+		});
+	});
+}
+
+function addApartment (request, response) {
+
+	var rented = request.body.rented;
+	var location = request.body.location;
+	var price = request.body.price;
+	var bed = request.body.bed;
+	var bath = request.body.bath;
+	var sqft = request.body.sqft;
+
+	newApart(rented, location, price, bed, bath, sqft, (function(error, result) {
+		if (error || result == null ) {
+			response.status(500).json({success: false, data: error});
+		} else {
+			response.status(200).json(result);
+		}
+	}));
+}
+
+function newApart(rented, location, price, bed, bath, sqft, callback){
+	var client = new pg.Client(connectionString);
+
+	client.connect(function(err) {
+		if (err) {
+			console.log("Error connecting to DB: ")
+			console.log(err);
+			callback(err, null);
+		}
+		var sql = "INSERT INTO apartment (rented, location, price, bed, bath, sqft) Values ($1, $2, $3, $4, $5, $6)";
+		var params = [rented, location, price, bed, bath, sqft];
+
+		var query = client.query(sql, params, function(err, result) {
+			client.end(function(err) {
+				if (err) throw err;
+			});
+
+			if (err) {
+				console.log("Error in query: ")
+				console.log(err);
+				callback(err, null);
+			}
+			console.log(result.rows);
+			callback(null, result.rows);
+		});
+	});
+}
+function available (request, response) {
+	availableApartments(function(error, result) {
+		if (error || result == null ) {
+			response.status(500).json({success: false, data: error});
+		} else {
+			response.status(200).json(result);
+		}
+	});
+}
+
+function availableApartments(callback){
+	var client = new pg.Client(connectionString);
+
+	client.connect(function(err) {
+		if (err) {
+			console.log("Error connecting to DB: ")
+			console.log(err);
+			callback(err, null);
+		}
+
+		var sql = "SELECT * FROM apartment WHERE rented = false";
+
+		var query = client.query(sql, function(err, result) {
+			client.end(function(err) {
+				if (err) throw err;
+			});
+
+			if (err) {
+				console.log("Error in query: ")
+				console.log(err);
+				callback(err, null);
+			}
+			console.log(result.rows);
+			callback(null, result.rows);
+		});
+	});
+}
+function apartmentNumber (request, response) {
+	var id = request.query.id;
+	apartmentSpecific(id, function(error, result) {
+		if (error || result == null ) {
+			response.status(500).json({success: false, data: error});
+		} else {
+			response.status(200).json(result);
+		}
+	});
+}
+function apartmentSpecific(id, callback){
+	var client = new pg.Client(connectionString);
+	client.connect(function(err) {
+		if (err) {
+			console.log("Error connecting to DB: ")
+			console.log(err);
+			callback(err, null);
+		}
+
+		var sql = "SELECT * FROM apartment WHERE apartment_id = $1::int";
+		console.log(id);
+		var params = [id];
+
+		var query = client.query(sql, params, function(err, result) {
+			client.end(function(err) {
+				if (err) throw err;
+			});
+
+			if (err) {
+				console.log("Error in query: ")
+				console.log(err);
+				callback(err, null);
+			}
+			console.log(result.rows);
+			callback(null, result.rows);
+		});
+	});
+}
+function editApartment (request, response) {
+	var id = request.body.id;
+	var rented = request.body.rented;
+	var location = request.body.location;
+	var price = request.body.price;
+	var bed = request.body.bed;
+	var bath = request.body.bath;
+	var sqft = request.body.sqft;
+
+	updateApart(id, rented, location, price, bed, bath, sqft, (function(error, result) {
+		if (error || result == null ) {
+			response.status(500).json({success: false, data: error});
+		} else {
+			response.status(200).json(result);
+		}
+	}));
+}
+function updateApart(id, rented, location, price, bed, bath, sqft, callback){
+	var client = new pg.Client(connectionString);
+
+	client.connect(function(err) {
+		if (err) {
+			console.log("Error connecting to DB: ")
+			console.log(err);
+			callback(err, null);
+		}
+		var sql = "UPDATE apartment SET rented = $2, location =$3, price=$4, bed=$5, bath=$6, sqft=$7 WHERE apartment_id = $1::int";
+		var params = [id, rented, location, price, bed, bath, sqft];
+
+		var query = client.query(sql, params, function(err, result) {
+			client.end(function(err) {
+				if (err) throw err;
+			});
+
+			if (err) {
+				console.log("Error in query: ")
+				console.log(err);
+				callback(err, null);
+			}
+			console.log(result.rows);
+			callback(null, result.rows);
+		});
+	});
 }
